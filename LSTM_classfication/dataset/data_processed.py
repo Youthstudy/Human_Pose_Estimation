4aaca756c8c85d7ee49d93439415642b87a34daa
Youthstudy <li18079088451@163.com>
2024-06-26 14:46:30 +0800

    1

--- LSTM_classfication/dataset/data_processed.py
+++ LSTM_classfication/dataset/data_processed.py
@@ -4,6 +4,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 from scipy.spatial.transform import Rotation as R
+import numpy as np
 
 # success
 def split_list(list):
@@ -11,7 +12,7 @@ def split_list(list):
     index_list = []
     s_list = []
     flag = 0
-    for i in list[0]:
+    for i in list[:,0]:
         try :
             temp.index(i)
         except ValueError:
@@ -22,15 +23,11 @@ def split_list(list):
             if temp[a] == i:
                 index_list[a].append(flag)
         flag += 1
-
+    
     for i in range(len(index_list)):
         s_list.append([])
-        a = 0
         for j in index_list[i]:
-            s_list[i].append([])
-            for k in range(len(list)):
-                s_list[i][a].append(list[k][j])
-            a += 1
+            s_list[i].append(list[j])
     return s_list
 
 def get_new_filename(filename):
@@ -57,9 +54,13 @@ def write_csv(data_write,savedir):
         name = f"{savedir}/{data_write[i][0][0]}"
         with open(chage_filename(name,0),'w',newline="") as csvfile:
             a = csv.writer(csvfile)
-            a.writerow(['SensorId', ' TimeStamp (s)', ' FrameNumber',' QuatW', ' QuatX', ' QuatY',' QuatZ',
-                        ' LinAccX (g)',' LinAccY (g)',' LinAccZ (g)',
-                        'rm0','rm1','rm2','rm3','rm4','rm5','rm6','rm7','rm8'])
+            a.writerow(['SensorId', 
+            #             ' AccX (g)',' AccY (g)',' AccZ (g)',
+            #    ' GyroX (deg/s)',' GyroY (deg/s)',' GyroZ (deg/s)',
+            #      ' MagX (uT)',' MagY (uT)',' MagZ (uT)',
+            #        ' EulerX (deg)',' EulerY (deg)',' EulerZ (deg)',
+               ' QuatW', ' QuatX', ' QuatY',' QuatZ',
+               ' LinAccX (g)',' LinAccY (g)',' LinAccZ (g)'])
             a.writerows(data_write[i])
 
 def creat_file(f_path):
@@ -81,45 +82,16 @@ def main():
 
     data = pd.read_csv(f"{f_path}")
     data = pd.DataFrame(data)
-    df = data[['SensorId', ' TimeStamp (s)', ' FrameNumber',' QuatW', ' QuatX', ' QuatY',' QuatZ',
+    df = data[['SensorId', 
+            #    ' AccX (g)',' AccY (g)',' AccZ (g)',
+            #    ' GyroX (deg/s)',' GyroY (deg/s)',' GyroZ (deg/s)',
+            #      ' MagX (uT)',' MagY (uT)',' MagZ (uT)',
+            #        ' EulerX (deg)',' EulerY (deg)',' EulerZ (deg)',
+               ' QuatW', ' QuatX', ' QuatY',' QuatZ',
                ' LinAccX (g)',' LinAccY (g)',' LinAccZ (g)']]
-
-    Sensor = df['SensorId'].to_list()
-    time = df[' TimeStamp (s)'].to_list()
-    FrameNumber = df[' FrameNumber'].to_list()
-    QuatW = df[' QuatW'].to_list()
-    QuatX = df[' QuatX'].to_list()
-    QuatY = df[' QuatY'].to_list()
-    QuatZ = df[' QuatZ'].to_list()
-    LinAccX = df[' LinAccX (g)'].to_list()
-    LinAccY = df[' LinAccY (g)'].to_list()
-    LinAccZ = df[' LinAccZ (g)'].to_list()
-
-    data_all = []
-    data_all.append(Sensor)
-    data_all.append(time)
-    data_all.append(FrameNumber)
-    data_all.append(QuatW)
-    data_all.append(QuatX)
-    data_all.append(QuatY)
-    data_all.append(QuatZ)
-    data_all.append(LinAccX)
-    data_all.append(LinAccY)
-    data_all.append(LinAccZ)
+    data_all = df.values
 
     writer = split_list(data_all)
-    
-    r = R.from_quat([writer[1][0][3],writer[1][0][4],writer[1][0][5],writer[1][0][6]])
-    print(writer[1][0][3],writer[1][0][4],writer[1][0][5],writer[1][0][6])
-    print(r.as_matrix())
-    for i in range(len(writer)):
-        for j in range(len(writer[i])):
-                temp = R.from_quat([writer[i][j][3],writer[0][i][4],writer[0][i][5],writer[0][i][6]])
-                temp = temp.as_matrix()
-                temp = temp.flatten()
-                for k in range(len(temp)):
-                    writer[i][j].append(temp[k])
-    
     write_csv(writer,savedir)
 
 
---
+++ LSTM_classfication/train_multpre.py
@@ -0,0 +1,92 @@
+import numpy as np
+import  pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+# # set GPU
+# tf.debugging.set_log_device_placement(True)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+# for gpu in gpus:
+#     tf.config.experimental.set_memory_growth(gpu, True)
+# print(len(gpus))
+# logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+# print(len(logical_gpus))
+
+# read data-sensor.csv
+a = pd.read_csv('dataset/singalimu_walk5ms.csv')
+dataframe = a[[' QuatW',' QuatX',' QuatY',' QuatZ',' LinAccX (g)',' LinAccY (g)',' LinAccZ (g)']]
+pd_value = dataframe.values
+
+# ========= split dataset ===================
+train_size = int(len(pd_value) * 0.8)
+trainlist = pd_value[:train_size]
+testlist = pd_value[train_size:]
+
+look_back = 10
+features = 7
+step_out = 1
+epochs = 100
+batch_size = 16
+predict_num = 10 # 多步预测
+
+# ========= numpy train ===========
+def create_dataset(dataset, look_back):
+#这里的look_back与timestep相同
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back)]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back + 1])
+    return np.array(dataX),np.array(dataY)
+#训练数据太少 look_back并不能过大
+trainX,trainY  = create_dataset(trainlist,look_back)
+testX,testY = create_dataset(testlist,look_back)
+
+# ========== set dataset ======================
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], features))
+testX = np.reshape(testX, (testX.shape[0], testX.shape[1] , features))
+
+# create and fit the LSTM network
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(64, activation='relu', return_sequences=True, input_shape=(look_back, features)))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.LSTM(32, activation='relu'))
+model.add(tf.keras.layers.Dense(features))
+#model.compile(optimizer='adam', loss='mse')
+model.compile(metrics=['accuracy'], loss='mean_squared_error', optimizer='adam')
+
+model.summary()
+
+history = model.fit(trainX, trainY, batch_size= batch_size,validation_data=(testX, testY),epochs=epochs, verbose=1).history
+model.save("model_data/lstm-model1.h5")
+
+plt.figure()
+plt.plot(history['loss'], linewidth=2, label='Train')
+plt.plot(history['val_loss'], linewidth=2, label='Test')
+plt.legend(loc='upper right')
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.savefig("model_loss.png")
+#plt.ylim(ymin=0.70,ymax=1)
+
+
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+for i in range(testPredict.shape[1]):
+    plt.figure()
+    plt.plot(testY[:,i],label = "real_data")
+    plt.plot(testPredict[:,i],label = "predict_data")
+    plt.legend(loc="lower right")
+    plt.title(f"{a[i]}")
+    plt.savefig(f"model_data/{a[i]}.png")
+    
+
+plt.show()
+
+
